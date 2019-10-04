8ae0008d4705928a4927f90e2a9fb1701bf904db
JoaoPauloETEC <joaojpmoreira25@gmail.com>
2019-10-04 17:26:35 -0300

    create components

--- src/app/somar/somar.module.ts
+++ src/app/somar/somar.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SomarComponent } from './components/somar/somar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SomarComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    SomarComponent
   ]
 })
 export class SomarModule { }
